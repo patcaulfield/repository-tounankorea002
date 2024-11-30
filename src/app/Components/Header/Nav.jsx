d3be342f6f40f85967054b32b8fc69ec46ac00a4
Giman Lee <patcaulfield@naver.com>
2024-11-30 19:00:47 +0900

    Nav 수정

--- src/app/Components/Header/Nav.jsx
+++ src/app/Components/Header/Nav.jsx
@@ -6,28 +6,7 @@ export default function Nav({ setMobileToggle }) {
     <ul className="cs_nav_list fw-medium">
 
       <li className="menu-item-has-children">
-        <Link href="/about" onClick={() => setMobileToggle(false)}>
-        About Us
-        </Link>
-        <DropDown>
-          <ul>
-            <li>
-              <Link href="/about" onClick={() => setMobileToggle(false)}>
-              About Light
-              </Link>
-            </li>
-            <li>
-              <Link href="/aboutdark" onClick={() => setMobileToggle(false)}>
-              About Dark
-              </Link>
-            </li>
-          </ul>
-        </DropDown>
-
-      </li>
-
-      <li className="menu-item-has-children">
-        <Link href="#">Pages</Link>
+        <Link href="#">About</Link>
         <DropDown>
           <ul>
             <li>
@@ -38,36 +17,6 @@ export default function Nav({ setMobileToggle }) {
             <li>
               <Link href="/team" onClick={() => setMobileToggle(false)}>
                Our Team
-              </Link>
-            </li>
-            <li>
-              <Link href="/testimonial" onClick={() => setMobileToggle(false)}>
-              Testimonial
-              </Link>
-            </li>
-            <li>
-              <Link href="/service" onClick={() => setMobileToggle(false)}>
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link href="/service/service-details" onClick={() => setMobileToggle(false)}>
-                Service Details
-              </Link>
-            </li>
-            <li>
-              <Link href="/project" onClick={() => setMobileToggle(false)}>
-                Project
-              </Link>
-            </li>
-            <li>
-              <Link href="/project/project-details" onClick={() => setMobileToggle(false)}>
-                 Project Details
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact" onClick={() => setMobileToggle(false)}>
-              Contact
               </Link>
             </li>
           </ul>
@@ -85,38 +34,8 @@ export default function Nav({ setMobileToggle }) {
               </Link>
             </li>
             <li>
-              <Link href="/projectdark" onClick={() => setMobileToggle(false)}>
-              Project Dark
-              </Link>
-            </li>
-            <li>
-              <Link href="/project/project-details" onClick={() => setMobileToggle(false)}>
-                Project Details Left
-              </Link>
-            </li>
-            <li>
-              <Link href="/project/project-details-right" onClick={() => setMobileToggle(false)}>
-              Project Details Right
-              </Link>
-            </li>
-            <li>
               <Link href="/project/project-details-center" onClick={() => setMobileToggle(false)}>
               Project Details Center
-              </Link>
-            </li>
-            <li>
-              <Link href="/projectdark/project-details-dark" onClick={() => setMobileToggle(false)}>
-                Project Details Left Dark
-              </Link>
-            </li>
-            <li>
-              <Link href="/projectdark/project-details-right-dark" onClick={() => setMobileToggle(false)}>
-                Project Details Right Dark
-              </Link>
-            </li>
-            <li>
-              <Link href="/projectdark/project-details-center-dark" onClick={() => setMobileToggle(false)}>
-                Project Detail Center Dark
               </Link>
             </li>
           </ul>
@@ -135,38 +54,8 @@ export default function Nav({ setMobileToggle }) {
               </Link>
             </li>
             <li>
-              <Link href="/servicedark" onClick={() => setMobileToggle(false)}>
-                Services Dark
-              </Link>
-            </li>
-            <li>
-              <Link href="/service/service-details" onClick={() => setMobileToggle(false)}>
-                Service Details Left
-              </Link>
-            </li>
-            <li>
-              <Link href="/service/service-details-right" onClick={() => setMobileToggle(false)}>
-                Service Details Right
-              </Link>
-            </li>
-            <li>
               <Link href="/service/service-details-center" onClick={() => setMobileToggle(false)}>
                 Service Details Center
-              </Link>
-            </li>
-            <li>
-              <Link href="/servicedark/service-details-dark" onClick={() => setMobileToggle(false)}>
-                Service Details Left Dark
-              </Link>
-            </li>
-            <li>
-              <Link href="/servicedark/service-details-right-dark" onClick={() => setMobileToggle(false)}>
-                Service Details Right Dark
-              </Link>
-            </li>
-            <li>
-              <Link href="/servicedark/service-details-center-dark" onClick={() => setMobileToggle(false)}>
-                Service Detail Center Dark
               </Link>
             </li>
           </ul>
@@ -185,11 +74,6 @@ export default function Nav({ setMobileToggle }) {
               </Link>
             </li>
             <li>
-              <Link href="/blogdark" onClick={() => setMobileToggle(false)}>
-                Blog Dark
-              </Link>
-            </li>
-            <li>
               <Link
                 href="/blog/blog-details"
                 onClick={() => setMobileToggle(false)}
@@ -197,37 +81,14 @@ export default function Nav({ setMobileToggle }) {
                 Blog Details Left
               </Link>
             </li>
-            <li>
-              <Link href="/blog/blog-details-right" onClick={() => setMobileToggle(false)}>
-              Blog Details Right
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog/blog-details-center" onClick={() => setMobileToggle(false)}>
-              Blog Details Center
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/blogdark/blog-details-dark"
-                onClick={() => setMobileToggle(false)}
-              >
-                Blog Details Left Dark
-              </Link>
-            </li>
-            <li>
-              <Link href="/blogdark/blog-details-right-dark" onClick={() => setMobileToggle(false)}>
-              Blog Details Right Dark
-              </Link>
-            </li>
-            <li>
-              <Link href="/blogdark/blog-details-center-dark" onClick={() => setMobileToggle(false)}>
-              Blog Detail Center Dark
-              </Link>
-            </li>
 
           </ul>
         </DropDown>
+      </li>
+      <li>
+        <Link href="/contact" onClick={() => setMobileToggle(false)}>
+          Contact
+        </Link>
       </li>
     </ul>
   );
